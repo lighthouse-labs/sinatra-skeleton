35b99a34e311d25fb04e14739a33ef1e1b7ef154
Khurram Virani <kvirani@gmail.com>
2014-03-22 10:12:08 -0700

    add pry

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ require 'active_support/all'
 require 'sinatra'
 require 'sinatra/activerecord'
 
+require 'pry'
+
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
 
