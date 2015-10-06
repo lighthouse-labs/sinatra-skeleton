53a78da4b96d201630912da1fa4278c34cea7897
aarens <april_arens@hotmail.com>
2015-10-06 16:09:06 -0700

    reformatted login form
    hid glyphicons
    removed fieldset (line around form)
    added a help link
    moved signup form to new file

--- app/actions.rb
+++ app/actions.rb
@@ -3,7 +3,39 @@ get '/' do
   erb :index
 end
 
+
+
 get '/login' do
     erb :login
 end
+
+post '/login'do
+  redirect '/'
+end
+
+
+
+get '/profile' do
+  erb :profile
+end
+
+post '/profile' do
+  redirect '/'
+end
+
+
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  redirect '/'
+end
+
+
+
+
+
+
 
