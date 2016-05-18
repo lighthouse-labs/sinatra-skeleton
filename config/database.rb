7387e804ed8c1ad7cde9b7084912422c762c85e7
Vaz Allen <v@tryptid.com>
2016-05-18 16:51:16 -0700

    Use RACK_ENV as usual instead of hard-coded db

--- config/database.rb
+++ config/database.rb
@@ -1,3 +1,6 @@
+ENV['RACK_ENV'] ||= 'development'
+DATABASE_PATH = APP_ROOT.join('db', "#{ENV['RACK_ENV']}.sqlite3")
+
 configure do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
@@ -6,7 +9,7 @@ configure do
 
   set :database, {
     adapter: "sqlite3",
-    database: "db/db.sqlite3"
+    database: DATABASE_PATH
   }
 
   # Load all models from app/models, using autoload instead of require
